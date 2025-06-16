f60fc1b9de4475320f66548be8d1679c116d6303
shakil <shakil.sarkar1932@gmail.com>
2025-06-17 01:42:40 +0600

    home slider create

---
+++ src/Components/Slider/Slider.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import {
+  A11y,
+  Autoplay,
+  Navigation,
+  Pagination,
+  Scrollbar,
+} from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import Slide from "../Slide/Slide";
+const Slider = () => {
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+      spaceBetween={50}
+      slidesPerView={1}
+      autoplay={{
+        delay: 3000,
+        disableOnInteraction: false,
+      }}
+      pagination={{ clickable: true }}
+    >
+      <SwiperSlide className="-z-50">
+        <Slide
+          bgImage="https://i.ibb.co/TMptqqFm/freepik-the-style-is-candid-image-photography-with-natural-46895.png"
+          header="Find What’s Lost, Faster!"
+          desc="Report lost items and get help from our trusted community. Every item matters."
+          btnName="View Lost&Found Item"
+          link="/lost&found"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Slide
+          bgImage="https://i.ibb.co/8DfhGzYg/freepik-the-style-is-candid-image-photography-with-natural-56379.png"
+          header="Your Lost Item is One Click Away"
+          desc="Discover recovered items and connect with finders instantly through our secure platform."
+          btnName="View Lost&Found Item"
+          link="/lost&found"
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <Slide
+          bgImage="https://i.ibb.co/JFbg3B55/freepik-a-diverse-group-of-friendly-people-holding-recover-44299.png"
+          header="Join Our Caring Community!"
+          desc="Be a part of a community where people help each other reunite with their lost belongings. Together, we make the world a little safer and kinder."
+          btnName="Join Our Community"
+          link="/login"
+        />
+      </SwiperSlide>
+    </Swiper>
+  );
+};
+
+export default Slider;
